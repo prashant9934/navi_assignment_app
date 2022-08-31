41231053e56092b8048f4a1670b5b6a76a6ed3a3
prashant9934 <krprashant146@gmail.com>
2022-08-31 17:18:04 +0530

    added dependency for unit test

---
+++ app/src/test/java/com/example/naviassignmentapp/data/PullRequestPagingDataSourceTest.kt
@@ -0,0 +1,10 @@
+package com.example.naviassignmentapp.data
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+
+class PullRequestPagingDataSourceTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+}
---
+++ app/src/test/java/com/example/naviassignmentapp/viewmodel/ClosedPullRequestViewModelTest.kt
@@ -0,0 +1,5 @@
+package com.example.naviassignmentapp.viewmodel
+
+class ClosedPullRequestViewModelTest {
+
+}
